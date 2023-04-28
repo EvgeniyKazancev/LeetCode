085aff90a1d460adf1130b70a8f9842ec0dcdf1f
EvgeniyKazancev <ekazancev777@gmail.com>
2023-04-28 18:40:08 +0300

    Dobavil

--- src/main/java/Solution8.java
+++ src/main/java/Solution8.java
@@ -1,27 +1,23 @@
+import java.util.Arrays;
+
 public class Solution8 {
+    public static void main(String[] args) {
+        Solution8 solution8 = new Solution8();
+        int[] nums1 = {1, 1, 4, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        int[] arr = new int[m + n];
+        solution8.merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
+
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] res = new int[m + n];
-        int a;
-        int b;
-        for (int i = 0, i1 = 0, i2 = 0; i < m + n; i++ ) {
-            if (i1 >= nums1.length) {
-                a = Integer.MAX_VALUE;
-            } else {
-                a = nums1[i1];
-            }
-            if (i2 >= nums2.length){
-                b = Integer.MAX_VALUE;
-            }else {
-                b = nums2[i2];
-            }
-            if (a < b) {
-                res[i] = a;
-                i1++;
-            } else {
-                res[i] = b;
-                i2++;
-            }
+        for (int i = n, j = 0; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
         }
-        nums1 = res;
+      Arrays.sort(nums1);
     }
 }
+
