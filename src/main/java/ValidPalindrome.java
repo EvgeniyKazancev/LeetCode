a617ec31cf75a9b2b242c19158bb06ef41fd1aaa
EvgeniyKazancev <ekazancev777@gmail.com>
2023-06-28 16:22:00 +0300

    Refresh

---
+++ src/main/java/ValidPalindrome.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        ValidPalindrome vp = new ValidPalindrome();
+        String s = "A man, a plan, a canal: Panama";
+        boolean result = vp.isPalindrome(s);
+        System.out.println(result);
+    }
+
+    public boolean isPalindrome(String s) {
+        String sl = s.toLowerCase();
+        String revers = "";
+        String reversSb = "";
+
+        if (s == " ") {
+            return true;
+        }
+        for (int i = sl.length() - 1; i > -1; i--) {
+            char c = sl.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                revers += c;
+
+            }
+        }
+
+        StringBuilder sb = new StringBuilder(revers);
+        reversSb = sb.reverse().toString();
+        if (revers.equals(reversSb) ) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
