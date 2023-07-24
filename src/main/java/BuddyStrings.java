96dec3e4518a016ab5db5ba91a048b2e5d70c136
EvgeniyKazancev <ekazancev777@gmail.com>
2023-07-24 19:36:18 +0300

    Refresh

--- src/main/java/BuddyStrings.java
+++ src/main/java/BuddyStrings.java
@@ -1,30 +1,34 @@
-import static java.lang.String.valueOf;
+
 
 public class BuddyStrings {
     public static void main(String[] args) {
         BuddyStrings bs = new BuddyStrings();
-        String s = "aaaacb";
-        String goal = "aaaabc";
+        String s = "abab";
+        String goal = "abab";
         boolean result = bs.buddyStrings(s, goal);
         System.out.println(result);
     }
 
     public boolean buddyStrings(String s, String goal) {
-        if (s.length() != goal.length()){
+        if (s.length() != goal.length()) {
             return false;
         }
+
         String res = "";
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length() -1; i++) {
-              if (s.charAt(i) != goal.charAt(i)){
-                 sb.append(goal.charAt(i));
-                 sb.append(s.charAt(i));
-            }else
-                sb.append(s.charAt(i));
+        StringBuilder uni = new StringBuilder();
 
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != goal.charAt(i)) {
+                uni.append(s.charAt(i));
+            } else
+                sb.append(s.charAt(i));
+        }
+        if (uni.isEmpty()) {
+            sb.reverse();
         }
         res = sb.toString();
-
+        res += String.valueOf(uni.reverse());
         if (res.equals(goal)) {
             return true;
         } else
