0cf8359da1005b10897a08a9e7648bd0b3a4bcd2
EvgeniyKazancev <ekazancev777@gmail.com>
2023-08-27 18:26:44 +0300

    Refresh

---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,24 @@
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {1,3,7,9};
+        int target = 8;
+        System.out.println();
+    }
+    public int[] twoSum(int[] nums, int target) {
+
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1 ; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i,j};
+
+                }
+            }
+        }
+        return null;
+    }
+}
