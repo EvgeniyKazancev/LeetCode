c8932acc729d7e1d2191e1effc6b471328f713dc
EvgeniyKazancev <ekazancev777@gmail.com>
2023-10-23 19:09:50 +0300

    Refresh

--- src/main/java/PreorderTraversal.java
+++ src/main/java/PreorderTraversal.java
@@ -1,33 +1,52 @@
-import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PreorderTraversal {
-     class TreeNode {
-     int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-      }
-      TreeNode(int val) {
-          this.val = val;
-      }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
+        TreeNode() {
 
-  }
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> integerList = new ArrayList<>();
-       if (root != null){
-           integerList.add(root.val);
-          integerList.addAll(preorderTraversal(root.left));
-          integerList.addAll(preorderTraversal(root.right));
+        if (root != null) {
+            integerList.add(root.val);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
 
-       }
-      return integerList;
+        }
+        return integerList;
+
+
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> integerList = new ArrayList<>();
+
+        if (root != null) {
+            postorderTraversal(root.left);
+
+            postorderTraversal(root.right);
+
+            integerList.add(root.val);
+        }
+        return integerList;
+
     }
 }
