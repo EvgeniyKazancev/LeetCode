5a608d317ed9e7da5dd940f7d016c3ae6458c8f4
EvgeniyKazancev <ekazancev777@gmail.com>
2024-02-06 21:28:02 +0300

    Refresh

--- src/main/java/CombinationSum.java
+++ src/main/java/CombinationSum.java
@@ -10,14 +10,15 @@ public class CombinationSum {
 
     private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] candidates, int remain, int start) {
         if (remain < 0) {
-            return;
+           return;
         } else if (remain == 0) {
             result.add(new ArrayList<>(tempList));
         } else {
             for (int i = start; i < candidates.length; i++) {
                 tempList.add(candidates[i]);
+                System.out.println(tempList);
                 backtrack(result, tempList, candidates, remain - candidates[i], i);
-                tempList.remove(tempList.size() - 1);
+               tempList.remove(tempList.size() - 1 );
             }
         }
     }
