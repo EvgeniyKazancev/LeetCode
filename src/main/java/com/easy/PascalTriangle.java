2cc45688680865d0910fd3d9f15182b32a32d8fd
EvgeniyKazancev <ekazancev777@gmail.com>
2025-02-19 19:38:45 +0300

    решение задачи 119. Pascal's Triangle II

--- src/main/java/com/easy/PascalTriangle.java
+++ src/main/java/com/easy/PascalTriangle.java
@@ -24,4 +24,23 @@ public class PascalTriangle {
         }
         return result;
     }
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+        result.add(row);
+        for (int i = 1; i < rowIndex + 1; i++) {
+            List<Integer> temp = new ArrayList<>();
+            temp.add(1);
+            for (int j = 1; j < i; j++) {
+                temp.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+
+            }
+            temp.add(1);
+            result.add(temp);
+        }
+
+        return result.get(rowIndex);
+    }
 }
